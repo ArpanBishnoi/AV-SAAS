82aaca792f8282169e4be27f8e89c7adec42fa2c
ArpanBishnoi <vishnoiarpan61@gmail.com>
2026-07-04 10:52:23 +0530

    Add files via upload

---
+++ Backend.py
@@ -0,0 +1,250 @@
+import os
+import bcrypt
+from dotenv import load_dotenv
+from pydantic import BaseModel,EmailStr,Field
+load_dotenv()
+print(os.getcwd()) 
+print(os.getenv('Gemini_API_KEY'))
+from google import genai
+from jose import jwt,JWTError
+from fastapi import Depends
+from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
+from datetime import datetime, timedelta
+gemini_client = genai.Client(api_key=os.getenv("Gemini_API_KEY"))
+import uvicorn
+from fastapi import FastAPI, HTTPException
+import sqlite3
+import chromadb
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+SECRET_KEY = "super_secret_key_change_later"
+ALGORITHM = "HS256"
+security = HTTPBearer()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials# credentials = HTTPAuthorizationCredentials(scheme="Bearer",credentials="eyJHbGc..."
+#credentials create a list like this 
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        user_id = int(payload['sub'])
+        return user_id
+    except JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail= 'invalid or expired token'
+        )
+
+def create_access_token(user_id):
+    expire = datetime.utcnow() + timedelta(days=1)
+    payload = {"sub": str(user_id), "exp": expire}
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+
+@app.get("/")
+def home():
+    return {"message": "API is running"}
+
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed.decode()
+
+
+def verify_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+ 
+conn = sqlite3.connect("Saas.db", check_same_thread=False)
+cursor = conn.cursor()
+conn.commit()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cursor.fetchall())
+client = chromadb.PersistentClient(path="chroma_db")
+notes_collection = client.get_or_create_collection(name="NOTEs")
+memory_collection = client.get_or_create_collection(name="MEMOORY")
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE NOT NULL,email TEXT UNIQUE NOT NULL,password TEXT NOT NULL)"
+)
+conn.commit()
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS Notes(id INTEGER PRIMARY KEY AUTOINCREMENT,user_id INTEGER ,content TEXT)"
+)
+conn.commit()
+
+
+def add_users(username, email, password):
+    cursor.execute(
+        "INSERT INTO Users(username,email,password) VALUES(?,?,?)",
+        (username, email, password),
+    )
+    conn.commit()
+
+
+def login_users(username, email, password):
+    cursor.execute(
+        "SELECT * FROM Users WHERE username = ? AND email = ?", (username, email)
+    )
+    user = cursor.fetchone()
+    if user and verify_password(password, user[3]):
+        return user
+    else:
+        return None
+
+
+def add_notes(user_id, content):
+    cursor.execute("INSERT INTO Notes(user_id,content) VALUES(?,?)", (user_id, content))
+    conn.commit()
+    note_id = cursor.lastrowid
+    notes_collection.add(
+        documents=[content], ids=[str(note_id)], metadatas=[{"user_id": user_id}]
+    )
+
+
+def get_notes(user_id):
+    cursor.execute("SELECT * FROM Notes WHERE user_id = ?", (user_id,))
+    return cursor.fetchall()
+
+
+def search_notes(user_id, question):
+    results = notes_collection.query(
+        query_texts=[question], where={"user_id": user_id}, n_results=2
+    )
+    return results["documents"][0]
+
+
+def generate_answer(prompt):
+    response = gemini_client.models.generate_content(
+        model="gemini-2.5-flash", contents=prompt
+    )
+    return response.text
+
+
+def ask_ai(user_id, question):
+    retrieved_notes = search_notes(user_id, question)
+    if not retrieved_notes:
+        raise HTTPException(status_code=404, detail="No notes found for this user")
+    retrieved_memory = search_memory(user_id, question)
+    notes_context = "\n".join(retrieved_notes)
+    if retrieved_memory:
+        memory_context = "\n".join(retrieved_memory)
+    else:
+        memory_context = ""
+    prompt = f"""
+    You are a helpful AI assistant.
+    use the user's notes and memory to answer the question.
+    Notes:
+    {notes_context}
+    Memory:
+    {memory_context}
+    Notes context:
+    {notes_context}
+    Question: {question}
+    Give a helpful answer based only on provided context.
+    """
+    answer = generate_answer(prompt)
+    memory_text = f"""
+     Question:
+    {question}
+     Answer:
+    {answer}
+     """
+    memory_collection.add(
+        documents=[memory_text],
+        ids=[str(cursor.lastrowid)],
+        metadatas=[{"user_id": user_id}],
+    )
+    return answer
+
+
+def search_memory(user_id,question):
+    results = memory_collection.query(
+        query_texts=[question], where={"user_id": user_id}, n_results=2
+    )
+    return results["documents"][0]
+
+class UPDATENOTEINPUT(BaseModel):
+    id: int
+    content: str=Field(min_length=1)
+
+
+class USERINPUT(BaseModel):
+    username: str = Field(min_length=3,max_length=30)
+    email: str = Field(min_length=5,max_length=60)
+    password: str = Field(min_length=5,max_length=10)
+
+
+class LOGINUSER(BaseModel):
+    username: str
+    email: str
+    password: str
+class NOTEINPUTJWT(BaseModel):
+    content : str=Field(min_length=1)
+class QuestionInputJWT(BaseModel):
+     question : str=Field(min_length=1)   
+@app.post("/Register")
+def create_user(item: USERINPUT):         
+    try:
+        print("REGISTER POINT HIT")
+        print(item)
+        hashed_password = hash_password(item.password)
+        add_users(item.username, item.email, hashed_password)
+        return {"message": "User added successfully!!!"}
+    except Exception as e:
+        print("register error", e)
+        return {"error": str(e)}
+
+
+@app.post("/login")
+def login(item: LOGINUSER):
+    user = login_users(item.username, item.email, item.password)
+    if user:
+        token = create_access_token(user[0])
+        return {"message": "logined susccessfully", "user_id": user[0], "token": token}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+@app.post("/notes")
+def create_note(item: NOTEINPUTJWT,user_id: int =Depends(get_current_user)):
+    try:
+        print("ENDPOINT REACHED")
+        print(item)
+        add_notes(user_id, item.content)
+        return {"message": "Note Added !!!"}
+    except Exception as e:
+        print("ERROR:", e)
+        return {"error": str(e)}
+
+
+@app.post("/ask")
+def ask(item: QuestionInputJWT,user_id : int = Depends(get_current_user)):
+    response = ask_ai(user_id, item.question)
+    return {"response": response}
+
+@app.get("/read_notes")
+def read_notes(user_id: int = Depends(get_current_user)):
+    notes = get_notes(user_id)
+    if not notes:
+        raise HTTPException(status_code=404, detail="Notes not found")
+    return {"notes": notes}
+
+
+@app.put("/update notes")
+def update_notes(item: UPDATENOTEINPUT,user_id : int = Depends(get_current_user)):
+    cursor.execute("UPDATE Notes SET content = ? WHERE id = ? AND user_id = ?", (item.content, item.id,user_id))
+    conn.commit()
+    return {"message": "Note Updated !!!"}
+
+
+@app.delete("/Delete notes")
+def delete_notes(delete_id: int,user_id : int = Depends(get_current_user)):
+    cursor.execute(
+        "Delete FROM Notes WHERE id =? and user_id = ?", (delete_id,user_id)
+    )  # rememeber that we use i in place of user_id cause id increases but with use_id =1 we can have thousands of users so all will be deleted as id keep increasing but user_id can be shared by many users
+    conn.commit()
+    return {"message": "Note Deleted !!!"}
+
